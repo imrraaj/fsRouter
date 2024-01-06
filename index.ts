8ad8807bcd64ed9ce6d5df6daf2aaa823d2c1ebd
Raj Patel <riskyrick20@gmail.com>
2024-01-06 14:33:45 +0530

    renamed the class file

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { FSRouter } from "./router";
+import { FSRouter } from "./FSRouter";
 
 const app = express();
-const router = new FSRouter(app)
+const router = new FSRouter(app);
 router.useFileSystemRouting();
 
 app.get("/", (req, res) => { res.send("hi") });
-app.listen(3000, () => { console.log("Server Started") })
+app.listen(3000, () => { console.log("Server started on http://localhost:3000") })
 
